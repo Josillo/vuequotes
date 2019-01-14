1e62665d32699fa9cfe791e4fea1a4332843fc22
Jose An <jose.jaramago101@gmail.com>
2019-01-14 16:01:24 +0100

    Initial commit

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Server status {{ status }}</p>
+    <button @click="changeStatus">ChangeStatus</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    changeStatus: function() {
+      this.status = "Normal";
+    }
+  }
+};
+</script>
